52a29a30283e7a72b27529797003d62103cdaff4
James Seppi <james.seppi@gmail.com>
2012-06-04 23:56:30 -0500

    converted to wordpress theme

---
+++ sidebar.php
@@ -0,0 +1,8 @@
+<div class="span3 right-sidebar">
+    
+    <ul class="sidebar">
+    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('primary_sidebar') ) : ?>
+    
+    <?php endif; ?>
+    </ul>
+</div>
